4b39cd8d418d4258df3aec53be84df8673e9b2d4
rosswilliamsdev <116531898+rosswilliamsdev@users.noreply.github.com>
2023-02-26 10:45:36 -0600

    Search function

--- JS/app.js
+++ JS/app.js
@@ -1,13 +1,63 @@
 baguetteBox.run('.gallery');
 
 
-function searchBar() {
-    let input = document.getElementById('search').addEventListener('keyup');
-    let filter = input.value.toLowerCase();
-    let img = a.getElementsByTagName('img');
+const search = new Filter('search', 'data-caption');
 
-    for (i=0; i<img.length; i++) {
-        
-    }
-}
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const photoSearch = document.getElementById("search");
+
+// photoSearch.addEventListener("keyup", e => {
+//     let currentValue = e.target.value.toLowerCase();
+//     let photos = document.querySelectorAll("img");
+//     let captions = photos.getAttribute("data-caption");
+
+//     photos.forEach(photo => {
+//         if (captions.toLowerCase().includes(currentValue)) {
+//             photo.style.display = "block";
+//         } else {
+//             photo.style.display = "none";
+//         }
+//     });
+// });
+
+// photosearch.addEventListener("keyup", e => {
+//     let currentValue = e.target.value.toLowerCase();
+//     let photos = document.querySelectorAll("img");
+//     photos.forEach(photo => {
+//         if (photo.getAttribute("data-caption").toLocaleLowerCase().includes(currentValue)) {
+//             photo.style.display = "block";
+//         } else {
+//             photo.style.display = "none";
+//         }
+// });
+
+
+
+    //     for (let i = 0; i < 12; i++) {
+    //     photoArr.push(photos[i].data-caption)
+    // }
+    // for (let i = 0; i < photoArr.length; i++)
+    //     photo = photoArr[i];
+    //     result = photo.includes(photoSearch)
+    // });
---
+++ JS/mySearch.js
@@ -0,0 +1,54 @@
+const photoSearch = document.getElementById("search");
+
+photoSearch.addEventListener("keyup", e => {
+    let currentValue = e.target.value.toLowerCase();
+    let photos = document.querySelectorAll("img");
+    let captions = img.getAttribute("data-caption");
+
+    photos.forEach(photo => {
+        if (captions.toLowerCase().includes(currentValue)) {
+            photo.style.display = "block";
+        } else {
+            photo.style.display = "none";
+        }
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// function search() {
+//     let searchInput = document.getElementById("search").value;
+//     searchInput = searchInput.toLowerCase();
+
+//     const imageArr = [];
+//     let image, result;
+
+//     let images = document.querySelectorAll("img");
+//     let links = document.getElementsByTagName("a");
+
+//     for (let i = 0; i<12; i++) {
+//         imageArr.push(images[i].image)
+//     }
+
+//     for (let i = 0; i<imageArr.length; i++) {
+//        image = imageArr[i];
+//        result = image.includes(searchInput);
+
+//        if (!result) {
+//         images[i].style.display = "none";
+//         links[i].style.order = "-1";
+//        } else {
+//         images[i].style.display = "block";
+//         links[i].style.display = "1";
+//        }
+//     }
+// }
